f2222a7afdf6a9a999544a77846daf8b85ad5a14
todun <todun@users.noreply.github.com>
2019-08-07 15:27:28 +0000

    work in progress

---
+++ README.md
@@ -0,0 +1,31 @@
+# Massira Forum
+A forum for communicating about reproductive health.
+
+# Technology stack
+
+- VueJS
+- Firebase
+- PWA
+
+### setup
+This shows how to use the app on your local machine
+``` bash
+npm install # install dependencies
+
+npm run serve # start local server localhost:8080
+```
+
+### production deploy to firebase
+This shows how to deploy the app to firebase production environment
+
+```bash
+# create the production build
+npm run build
+```
+
+```bash
+# deploy production build to firebase
+firebase login
+firebase init;
+firebse deploy
+```
---
+++ src/firebaseConfig.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// firebase init goes here
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+const currentUser = auth.currentUser
+
+// firebase collections
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+export {
+    db,
+    auth,
+    currentUser,
+    usersCollection,
+    postsCollection,
+    commentsCollection,
+    likesCollection
+}
